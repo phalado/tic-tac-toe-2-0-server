ae03928048fbccb1f4462a80d52113a1bfa96bea
Rapha <phalado@gmail.com>
2022-11-25 23:34:25 -0300

    Add cors

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ const { createServer } = require('http')
 const { Server, Socket } = require('socket.io')
 
 const httpServer = createServer()
-const io = new Server(httpServer, { cors: { origin: '*' } })
+const io = new Server(httpServer, { cors: { origin: '*', allowHeaders: '*' } })
 
 interface Game {
   (gameId: string): {
