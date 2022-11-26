db97ae155d8dfd0ea4f94421dd934a23e9d2577a
Rapha <phalado@gmail.com>
2022-11-25 22:48:28 -0300

    Add a test to connection

--- server.ts
+++ server.ts
@@ -16,6 +16,8 @@ interface Game {
 let games: any = {}
 
 io.on('connection', (socket: any) => {
+  socket.on('testConnection', () => io.emit('connectionTested', "Server on"))
+
   socket.on('createGame', ({ playerOne }: { playerOne: string }) => {
     let gameId: string = ''
     for (let i = 0; i < 5; i++) gameId += (Math.floor(Math.random() * 10)).toString()
