2f51772e7f81a6e338f566c921e6350eee499f73
Rapha <phalado@gmail.com>
2022-11-29 15:11:11 -0300

    Changes

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ const { createServer } = require('http')
 const { Server } = require('socket.io')
 
 const httpServer = createServer()
-const io = new Server(httpServer, { cors: { origin: true } })
+const io = new Server(httpServer, { cors: { origin: '*', 'Access-Control-Allow-Origin': '*' } })
 
 let games: any = {}
 
