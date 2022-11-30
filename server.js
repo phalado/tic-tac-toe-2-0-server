caf1787bc471ffc245c526f7200c7b6dc2da71bf
Rapha <phalado@gmail.com>
2022-11-30 10:26:53 -0300

    Server working fine, emits being sent to correct users

--- server.js
+++ server.js
@@ -1,42 +1,60 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var createServer = require('http').createServer;
 var Server = require('socket.io').Server;
 var httpServer = createServer();
-var io = new Server(httpServer, { cors: { origin: '*', 'Access-Control-Allow-Origin': '*' } });
+var io = new Server(httpServer, { cors: {
+        // origin: 'https://tic-tac-toe-2-0.herokuapp.com',
+        origin: true,
+        credentials: true
+    } });
 var games = {};
 io.on('connection', function (socket) {
     socket.on('testConnection', function () { return io.emit('connectionTested', "Server on"); });
     socket.on('createGame', function (_a) {
         var playerOne = _a.playerOne;
-        console.log("Game created by " + playerOne);
+        console.log("Game created by " + playerOne.name);
         var gameId = '';
         for (var i = 0; i < 5; i++)
             gameId += (Math.floor(Math.random() * 10)).toString();
         games[gameId] = {
             playerOne: playerOne,
-            playerTwo: '',
+            playerTwo: { name: '', id: '' },
             currentPlayer: Math.random() < 0.5,
             round: 0
         };
-        io.emit('gameCreated', { gameId: gameId, playerId: gameId + 'A' });
+        io.to(playerOne.id).emit('gameCreated', { gameId: gameId });
     });
     socket.on('joinGame', function (_a) {
         var gameId = _a.gameId, playerTwo = _a.playerTwo;
         var game = games[gameId];
         game.playerTwo = playerTwo;
         game.round = 1;
-        io.emit('gameStart', {
+        var data = {
             gameId: gameId,
             playerOne: game.playerOne,
             playerTwo: game.playerTwo,
             playerTurn: game.currentPlayer,
             round: game.round
-        });
+        };
+        io.to(game.playerOne.id).emit('gameStart', __assign({}, data));
+        io.to(game.playerTwo.id).emit('gameStart', __assign({}, data));
     });
     socket.on('move', function (data) {
         var game = games[data.gameId];
         game.round = data.round;
         game.currentPlayer = !game.currentPlayer;
-        io.emit('newMove', {
+        var id = game.currentPlayer ? game.playerOne.id : game.playerTwo.id;
+        io.to(id).emit('newMove', {
             gameId: data.gameId,
             playerTurn: game.currentPlayer,
             round: game.round,
@@ -47,6 +65,5 @@ io.on('connection', function (socket) {
         });
     });
 });
-var port = 1337;
-httpServer.listen(port);
-console.log('Listening on port ' + port + '...');
+var port = process.env.PORT || 1337;
+httpServer.listen(port, function () { return console.log('Listening on port ' + port + '...'); });
--- server.ts
+++ server.ts
@@ -3,58 +3,77 @@ const { Server } = require('socket.io')
 
 const httpServer = createServer()
 const io = new Server(httpServer, { cors: {
-  origin: 'https://tic-tac-toe-2-0.herokuapp.com',
+  // origin: 'https://tic-tac-toe-2-0.herokuapp.com',
+  origin: true,
   credentials: true
 } })
 
-let games: any = {}
+interface PlayerInterface {
+  name: string
+  id: string
+}
+
+interface GameInterface {
+  [gameId: string]: {
+    playerOne: PlayerInterface,
+    playerTwo: PlayerInterface,
+    currentPlayer: boolean,
+    round: number
+  }
+}
+
+interface MoveInterface {
+  gameId: string
+  round: number
+  player: boolean
+  pieceIndex: number
+  pieceValue: number
+  cell: number
+}
+
+let games: GameInterface = {}
 
 io.on('connection', (socket: any) => {
-  console.log(socket.handshake)
   socket.on('testConnection', () => io.emit('connectionTested', "Server on"))
 
-  socket.on('createGame', ({ playerOne }: { playerOne: string }) => {
-    console.log("Game created by " + playerOne)
+  socket.on('createGame', ({ playerOne }: { playerOne: PlayerInterface }) => {
+    console.log("Game created by " + playerOne.name)
     let gameId: string = ''
     for (let i = 0; i < 5; i++) gameId += (Math.floor(Math.random() * 10)).toString()
     games[gameId] = {
       playerOne,
-      playerTwo: '',
+      playerTwo: { name: '', id: '' },
       currentPlayer: Math.random() < 0.5,
       round: 0
     }
 
-    io.emit('gameCreated', { gameId, playerId: gameId + 'A' })
+    io.to(playerOne.id).emit('gameCreated', { gameId })
   })
 
-  socket.on('joinGame', ({ gameId, playerTwo }: { gameId: string, playerTwo: string }) => {
+  socket.on('joinGame', ({ gameId, playerTwo }: { gameId: string, playerTwo: PlayerInterface }) => {
     const game = games[gameId]
     game.playerTwo = playerTwo
     game.round = 1
 
-    io.emit('gameStart', {
+    const data = {
       gameId,
       playerOne: game.playerOne,
       playerTwo: game.playerTwo,
       playerTurn: game.currentPlayer,
       round: game.round
-    })
+    }
+
+    io.to(game.playerOne.id).emit('gameStart', { ...data })
+    io.to(game.playerTwo.id).emit('gameStart', { ...data })
   })
 
-  socket.on('move', (
-    data: {
-      gameId: string,
-      round: number,
-      player: boolean,
-      pieceIndex: number,
-      pieceValue: number,
-      cell: number
-    }) => {
+  socket.on('move', (data: MoveInterface) => {
     const game = games[data.gameId]
     game.round = data.round
     game.currentPlayer = !game.currentPlayer
+    const id = game.currentPlayer ? game.playerOne.id : game.playerTwo.id
 
-    io.emit('newMove', {
+    io.to(id).emit('newMove', {
       gameId: data.gameId,
       playerTurn: game.currentPlayer,
       round: game.round,
